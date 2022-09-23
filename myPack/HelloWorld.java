8e7f131a434359463931822dbd897d16a378daca
YatiDiyora13 <ydiyora@prominentpixel.com>
2022-09-23 12:59:12 +0530

    new chenges

--- myPack/HelloWorld.java
+++ myPack/HelloWorld.java
@@ -2,6 +2,7 @@ package myPack;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world to");
+
     }
 }
